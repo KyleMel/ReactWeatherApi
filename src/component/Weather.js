913b913346b6d63322f820de2af8e479ed97e4e5
Antwon Smith <87429364+antwon-smith@users.noreply.github.com>
2021-12-01 12:10:31 -0500

    Adds env file for API key

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const baseUrl = 'http://api.weatherapi.com/v1/forecast.json?';
-const apiKey = 'key=7df10c444e0540a4975204452212411';
+const apiKey = process.env.REACT_APP_API_KEY;
 
 export const getWeatherData = async (cityname) => {
   try {
-    const {data} = await axios.get(baseUrl + `${apiKey}&q=${cityname}`);
+    const {data} = await axios.get(baseUrl + `key=${apiKey}&q=${cityname}`);
     return data
   } catch(error) {
     throw error;
